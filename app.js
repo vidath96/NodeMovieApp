1776657e502adcaa53dd2129fe74940250d895fb
vidath96 <vidathhasantha96@gmail.com>
2023-05-26 15:18:29 +0530

    Node Module Update

--- app.js
+++ app.js
@@ -4,10 +4,16 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+const helmet = require('helmet')
+
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+// var usersRouter = require('./routes/users');
 
 var app = express();
+
+app.use(helmet({
+    contentSecurityPolicy: false,
+}));
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -20,7 +26,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+// app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
